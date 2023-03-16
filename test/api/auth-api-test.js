bb3b75c45223a74af52fa8ffc2ce3bb45d4c95b2
David Cotter <20062924@mail.wit.ie>
2023-03-16 16:13:24 +0000

    trying to get check unauthorised test to work

--- test/api/auth-api-test.js
+++ test/api/auth-api-test.js
@@ -5,7 +5,7 @@ import { maggie } from "../fixtures.js";
 
 suite("Authentication API tests", async () => {
   setup(async () => {
-    placemarkService.clearAuth();
+    await placemarkService.clearAuth();
     await placemarkService.createUser(maggie);
     await placemarkService.authenticate(maggie);
     await placemarkService.deleteAllUsers();
@@ -28,7 +28,7 @@ suite("Authentication API tests", async () => {
   });
 
   test("check Unauthorized", async () => {
-    placemarkService.clearAuth();
+    await placemarkService.clearAuth();
     try {
       await placemarkService.deleteAllUsers();
       assert.fail("Route not protected");
